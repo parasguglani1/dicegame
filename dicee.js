9a94d8e87c9211ecf0168046684e14a5e4093af4
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2021-12-13 16:01:08 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,7 +1,19 @@
-
-var n= Math.random();
-n= n*6;
-n= Math.floor(n) +1;
-var imagesource= "images/dice"+n+".png";
-var image1= document.querySelectorAll("img")[0];
+var n1 = Math.random();
+n1 = n1 * 6;
+n1 = Math.floor(n1) + 1;
+var imagesource = "images/dice" + n1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", imagesource);
+
+var n2 = Math.random();
+n2 = n2 * 6;
+n2 = Math.floor(n2) + 1;
+var imagesource = "images/dice" + n2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", imagesource);
+
+if (n1 > n2) {
+  document.querySelector("h1").innerHTML = "Player1 won";
+} else if (n1 < n2) {
+  document.querySelector("h1").innerHTML = "Player2 won";
+} else document.querySelector("h1").innerHTML = "tie";
